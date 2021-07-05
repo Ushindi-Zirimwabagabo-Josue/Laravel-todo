8e595740f60bcb884823678389078fdedb4e6b24
Ushindi-Zirimwabagabo-Josue <josueuzj9@gmail.com>
2021-07-05 04:38:18 +0300

    feat(ui): implement the frontend side

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form v-on:reloadlist="getList()" />
+        </div>
+        <list-view 
+            :items="items" 
+            v-on:reloadlist="getList()" 
+        />
+        </div>
+</template>
+
+<script>
+import addItemForm from './addItemForm.vue'
+import ListItem from './listItem.vue'
+import listView from './listView.vue'
+export default {
+    components:{
+        addItemForm,
+        listView,
+        ListItem
+    },
+    data() {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList (){
+            axios.get('api/items')
+            .then(response => {
+                this.items = response.data
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+        }
+    },
+    created() {
+        this.getList();
+    },
+}
+</script>
+
+<style scoped>
+    .todoListContainer{
+        width: 350px;
+        /* margin: auto;
+        position: absolute;
+        top: 50%;
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%); */
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin: -70px 0 0 -170px;
+    }
+
+    .heading{
+        background: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title{
+        text-align: center;
+    }
+</style>>
+    
